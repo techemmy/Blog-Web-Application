52fda7d8345cb4f35d94743291da9eb624223841
Emmanuel Oloyede <emmanueltopea@gmail.com>
2022-09-27 01:07:32 +0100

    correct spelling

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
-configuations = process.env;
+const configurations = process.env;
 
 const verifyToken = (req, res, next) => {
   const token = req.cookies.jwt;
@@ -9,7 +9,7 @@ const verifyToken = (req, res, next) => {
     // return res.status(403).send("A token is required for authentication");
     return res.redirect("/auth/login");
   }
-  jwt.verify(token, configuations.TOKEN_KEY, (err, decodedToken) => {
+  jwt.verify(token, configurations.TOKEN_KEY, (err, decodedToken) => {
     if (err) {
       console.log(err);
       res.redirect("/auth/login");
@@ -23,7 +23,7 @@ const checkUser = (req, res, next) => {
   const token = req.cookies.jwt;
   res.locals.errors = null; // sets errors to null for authentication pages get requests e.g Login get request page
   if (token) {
-    jwt.verify(token, configuations.TOKEN_KEY, (err, decodedToken) => {
+    jwt.verify(token, configurations.TOKEN_KEY, (err, decodedToken) => {
       if (err) {
         console.log(err);
         res.locals.user = null;
